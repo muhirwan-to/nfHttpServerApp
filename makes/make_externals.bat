e8548b72ccea1dae253c861e5f405a6c031b75db
Muhammad Irwanto <muhammad.irwanto95@gmail.com>
2021-04-10 11:12:04 +0700

    - add opencv/modules as external

--- makes/make_externals.bat
+++ makes/make_externals.bat
@@ -6,7 +6,7 @@ echo ===========================================================
 
 set DEST_FOLDER=%~dp0\..\extern
 
-svn propset svn:externals -F externals.list %DEST_FOLDER%
+svn propset svn:externals -F ..\externals.list %DEST_FOLDER%
 svn update %DEST_FOLDER%
 
 echo .
